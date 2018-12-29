c56f7124fc4419110c726103093826e9f2a47db5
Bilal Ahmed <bilalahmedim@gmail.com>
2018-12-29 23:37:46 +0545

    nav-menu-icon complete

--- App/js/App.js
+++ App/js/App.js
@@ -3,7 +3,11 @@ var
 nav_icon = document.querySelector('.nav-menu');
 
 function navExpand(){
-  console.log("Nav Expanded");
+  if(nav_icon.classList[1] == undefined){
+    nav_icon.classList.add('nav-menu--close');
+  }else{
+    nav_icon.classList.remove('nav-menu--close');
+  }
 }
 
 nav_icon.addEventListener('click', navExpand);
