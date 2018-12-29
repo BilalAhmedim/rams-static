b4966d36f5de29d4c2d6d25d91a28e4c6e03c330
Bilal Ahmed <bilalahmedim@gmail.com>
2018-12-29 22:24:46 +0545

    navigation work in progress

---
+++ App/js/App.js
@@ -0,0 +1,9 @@
+// navigation on click
+var
+nav_icon = document.querySelector('.nav-menu');
+
+function navExpand(){
+  console.log("Nav Expanded");
+}
+
+nav_icon.addEventListener('click', navExpand);
--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,50 @@ var
 gulp = require('gulp'),
 postcss = require('gulp-postcss'),
 cssimport = require('postcss-import'),
-nested = require('postcss-nestde'),
+nested = require('postcss-nested'),
 mixin = require('postcss-mixins'),
 vars = require('postcss-simple-vars'),
-rgbahex = require('postcss-hexrgba'),
+hexrgba = require('postcss-hexrgba'),
 watch = require('gulp-watch'),
 autoprefixer = require('autoprefixer'),
 sync = require('browser-sync').create(),
 rename = require('gulp-rename'),
-maps = require('gulp-sourcemaps')
+maps = require('gulp-sourcemaps'),
+plumber = require('gulp-plumber')
 ;
+gulp.task('watch', function(){
+
+  sync.init({
+    server: {
+      baseDir: './App/'
+    },
+    notify: false
+  });
+
+  watch('./App/index.html', function(){
+    sync.reload();
+  });
+  watch('./App/css/**/*.pcss', gulp.series('stream'));
+  watch('./App/js/**/*.js', function(){
+    sync.reload();
+  });
+});
 
 gulp.task('styles', function(){
-  return gulp.src('./App/css/**/*.pcss')
-  .pipe(postcss([cssimport, mixin, nested, vars, hexrgba]))
+  return gulp.src('./App/css/style.pcss')
   .pipe(maps.init())
+  .pipe(postcss([cssimport, mixin, nested, vars, hexrgba, autoprefixer]))
   .pipe(plumber())
   .pipe(rename(
     {
       extname: ".css"
     }
   ))
-  .pipe(maps.write('/'))
-  .pipe(gulp.dest(./))
+  .pipe(maps.write())
+  .pipe(gulp.dest('./App/'))
 });
+
+gulp.task('stream', gulp.series('styles', function(){
+  return gulp.src('./App/style.css')
+  .pipe(sync.stream());
+}))
