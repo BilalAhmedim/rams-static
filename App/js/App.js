6992766c59f392df45072f93e88348f07f4e2e44
Bilal Ahmed <bilalahmedim@gmail.com>
2019-01-03 12:28:00 +0530

    Navigation Complete.

--- App/js/App.js
+++ App/js/App.js
@@ -1,15 +1,22 @@
 // navigation on click
 var
 nav_icon = document.querySelector('.nav-menu'),
-nav_expand = document.querySelector('ul.nav-menu__item__sub'),
+nav_expanded = document.querySelector('.nav-menu__item'),
+nav_content_hide = document.querySelector('.nav-menu__content'),
+nav_content__show = document.querySelector('.nav-menu__content--show'),
 nav_sub__menu = document.querySelectorAll('ul li a.drop')
 ;
 
 function navExpand(){
   if(nav_icon.classList[1] == undefined){
-    nav_icon.classList.add('nav-menu--close');  
+    nav_icon.classList.add('nav-menu--close');
+    nav_expanded.classList.add('nav-menu__item--is-expend');
+    nav_content_hide.classList.add('nav-menu__content--show');
+
   }else{
     nav_icon.classList.remove('nav-menu--close');
+    nav_expanded.classList.remove('nav-menu__item--is-expend');
+    nav_content_hide.classList.remove('nav-menu__content--show');
   }
 }
 
